c9233ef7ed031fa0e0865259836af13546739957
eznirp <prinzemuyo0@gmail.com>
2026-03-28 00:21:53 +0800

    lab-act1

--- muyo-client/src/App.jsx
+++ muyo-client/src/App.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
 import './App.css';
-
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React App!</h1>
-        <p>
-          Name: [Your Prinze Lynard F. Muyo]<br />
-          Email: [Your prinzemuyo0@gmail.com]<br />
-          Other Personal Info: [ I like Mobile legends]
-        </p>
-      </header>
-    </div>
-  );
-}
 
+  return (
+<div className="App">
+<header className="App-header">
+<h1>Welcome to My React App!</h1>
+<p>
+
+          Name: Prinze Lynard F. Muyo <br />
+
+          Email: prinzemuyo0@gmail.com <br />
+
+          Other Personal Info: BSIT Student
+</p>
+</header>
+</div>
+
+  );
+
+}
+ 
 export default App;
+ 
